7e7e44696589bdfac7a2d6ee0ce2607869b32c0d
Ben Hughes <hughesbenm@gmail.com>
2025-01-13 21:47:27 -0600

    Initial commit, basic desktop app functionality with Electron and React

---
+++ src/frontend/App.tsx
@@ -0,0 +1,43 @@
+import { useCallback, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [nodeVersion, setNodeVersion] = useState<string | undefined>(undefined);
+
+  const updateNodeVersion = useCallback(
+    async () => setNodeVersion(await backend.nodeVersion("Hello from App.tsx!")),
+    []
+  );
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="./vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+        <button onClick={updateNodeVersion}>
+          Node version is {nodeVersion}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
