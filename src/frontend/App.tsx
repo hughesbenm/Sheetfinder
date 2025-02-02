13932cf933597f9139d74e4404c749b1960e0212
Ben Hughes <hughesbenm@gmail.com>
2025-02-02 00:47:59 -0600

    Experimenting with css, minimal text with input

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,9 +1,8 @@
 import { useCallback, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [charName, setCharName] = useState<string>("");
   const [nodeVersion, setNodeVersion] = useState<string | undefined>(undefined);
 
   const updateNodeVersion = useCallback(
@@ -12,31 +11,10 @@ function App() {
   );
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="./vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <button onClick={updateNodeVersion}>
-          Node version is {nodeVersion}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <div className="window">
+	  	<p>Text</p>
+	  	<input type="text" />
+	</div>
   );
 }
 
