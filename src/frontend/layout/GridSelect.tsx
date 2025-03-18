eaf0dc1df1b462f1849a7bdf6578dede3aec4f31
Ben Hughes <hughesbenm@gmail.com>
2025-03-17 22:23:36 -0500

    Switch from LabledSelect to GridSelect

--- src/frontend/layout/GridSelect.tsx
+++ src/frontend/layout/GridSelect.tsx
@@ -1,10 +1,11 @@
 import "./LabeledInput.css";
 import React from "react";
 import { GridOptionProps } from "./GridOption";
-import './LabeledSelect.css'
+import './GridSelect.css'
 import { Grid2 } from "@mui/material";
+import { GridSize } from "../types/layout";
 
-interface LabeledSelectProps<T extends string | number> {
+interface GridSelectProps<T extends string | number> {
 	position?: "top" | "bottom",
 	value: T;
 	className?: string,
@@ -12,16 +13,18 @@ interface LabeledSelectProps<T extends string | number> {
 	disabled?: boolean,
 	text?: string,
 	children: React.ReactElement<GridOptionProps>[] | React.ReactElement<GridOptionProps>;
+	size: GridSize;
 }
 
-const LabeledSelect = <T extends string | number>({
+const GridSelect = <T extends string | number>({
 	value,
 	className,
 	text,
 	disabled,
 	setValue,
-	children
-}: LabeledSelectProps<T>): JSX.Element => {
+	children,
+	size
+}: GridSelectProps<T>): JSX.Element => {
 
 	return <Grid2 className={`grid_item`} size={size}>
 		<div className={`labeled_select ${className}`}>
@@ -40,4 +43,4 @@ const LabeledSelect = <T extends string | number>({
 	</Grid2>
 }
 
-export default LabeledSelect;
+export default GridSelect;
